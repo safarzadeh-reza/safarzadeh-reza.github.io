ca8e5a72c09c8a854c50ce1093a0a69639375919
safarzadeh-reza <reza.sz70@gmail.com>
2020-05-22 18:45:07 +0430

    email

--- api/contact.php
+++ api/contact.php
@@ -3,9 +3,6 @@
   // FIXME: Update this to your desired email address.
   $configEmail = "rsafarzadeh@mail.kntu.ac.ir";
 
-  // My modifications to mailer script from:
-  // http://blog.teamtreehouse.com/create-ajax-contact-form
-
   // Added input sanitizing to prevent injection
   // Only process POST reqeusts.
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
